969bffb16c7181ca4f6a227af92135bb4696f453
mahadahmed35 <mahadahmed3528@gmail.com>
2026-09-14 19:13:11 +0200

    First Commit

---
+++ Console.js
@@ -0,0 +1,16 @@
+// let name = "Mahd";
+// console.log(typeof(function (){}));
+// String Indexing.
+// let name = ["Maame"];
+// name[0]="n"
+// console.log(name)
+//String Escaping.
+// let name = "I am mhad from home \"AKA mo \"What about you"
+// console.log(name)
+// let name = "GreatChoice";
+// for(let i = 0; i<=name.length; i++){
+//     console.log(name.charCodeAt(i))
+// }
+// Null Undefined
+var x = null;
+console.log(x)
