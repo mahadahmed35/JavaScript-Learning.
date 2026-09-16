fae1f620ad5afb4aefb7e86c99a5473cecc47f0b
mahadahmed35 <mahadahmed3528@gmail.com>
2026-09-16 17:35:00 +0200

    Switch JS Conditionals

--- Conditionals.js
+++ Conditionals.js
@@ -136,7 +136,56 @@
 // var preorder = true;
 
 // console.log("It costs $" + (adult ? "40.00" : "20.00") + " to attend the concert. Pick up your tickets at the " + (preorder ? "will call" : "gate") + ".");
-var eatPlants= true;
-var eatAnimals = false;
-var category = eatPlants? (eatAnimals? "omnivore" : "herbivore") : (eatAnimals? "carnivore" : "undefined");
-console.log(category)
+// var eatPlants= true;
+// var eatAnimals = false;
+// var category = eatPlants? (eatAnimals? "omnivore" : "herbivore") : (eatAnimals? "carnivore" : "undefined");
+// console.log(category)
+// var prize = "";
+// var winner= 1;
+
+// switch (winner) {
+//   case 1:
+//     prize += "a trip for two to the Bahamas and ";
+//     break;
+//   case 2:
+//     prize += "a four piece furniture set.";
+//     break;
+//   case 3:
+//     prize += "a smartwatch and ";
+//   default:
+//     prize += "tickets to the circus.";
+// }
+
+// console.log("You've won " + prize);
+// Matching education and average salary.
+var education = 'a high school diploma';
+
+// set the value of this based on a person's education
+var salary = 0;
+
+// your code goes here
+switch(education){
+    case "no high school diploma" :
+        salary = 25636;
+        break;
+        case "a high school diploma":
+            salary = 35256
+            break;
+            case "an Associate's degree":
+                salary = 41496;
+                break;
+                case "a Bachelor's degree":
+                    salary = 59124
+                    break;
+                    case "a Master's degree":
+                        salary = 69732;
+                        case "a Professional degree":
+                            salary = 89960;
+                            break;
+                            case "a Doctoral degree":
+                                salary= 84396;
+                break;
+}
+
+console.log("In 2015, a person with "+ education+" earned an average of $"+ salary.toLocaleString("en-US")+" /year."
+)
