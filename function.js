74193ec27274fcd2646006a212381f85f9c91dce
mahadahmed35 <mahadahmed3528@gmail.com>
2026-09-21 10:38:32 +0200

    Accumulator Function

--- function.js
+++ function.js
@@ -1,5 +1,16 @@
 // function declaration are ok with hoisting you can access them down to up.
-console.log( adder(2,3))
-function adder(a,b){
-    return a + b;
+// console.log( adder(2,3))
+// function adder(a,b){
+//     return a + b;
+// }
+// Sound.
+var sound = "" ; 
+function laugh(num) {
+    for (var x = 0 ; x < num ; x++) {
+        sound = sound + "ha" ;  
+    }
+    sound = sound +"!"; 
+    return sound; 
 }
+
+console.log(laugh(3)) ;
