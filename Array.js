a73fe4c775e89a03b83166a2ef309985c84ccf7f
mahadahmed35 <mahadahmed3528@gmail.com>
2026-09-23 17:53:36 +0200

    forEach patterns, elements, index, array

--- Array.js
+++ Array.js
@@ -12,11 +12,34 @@
 // console.log(hasEnoughPlayers(team));
 // // 
 // Array Methods.
-const numbers = [1, 2, 3];
-const doubles = [];
+// const numbers = [1, 2, 3];
+// const doubles = [];
 
-const doubled = numbers.forEach(function(num) {
-    doubles.push(num * 2)
+// const doubled = numbers.forEach(function(num) {
+//     doubles.push(num * 2)
 
-});
-console.log(doubles);
+// });
+// console.log(doubles);
+// Quiz: Another Type of Loop.
+// var test = [12, 929, 11, 3, 199, 1000, 7, 1, 24, 37, 4,
+//     19, 300, 3775, 299, 36, 209, 148, 169, 299,
+//     6, 109, 20, 58, 139, 59, 3, 1, 139
+// ];
+// test.forEach(function(num){
+//     if(num/3 === 0){
+//         return num + 100;
+//     }else{
+//         return "Not divisible by 3"
+//     }
+//     console.log(test)
+// })
+
+
+//
+const myArray = [1, 3, 4, 5, 6, 6]
+function myAwesomeFunction(element, index, array){
+    console.log("Element: " + element + "Array: " + index);
+    
+}
+
+myArray.forEach(myAwesomeFunction)
