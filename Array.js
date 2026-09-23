9bdb30f8f1d64f6fbbb66c86fa650c119164061f
mahadahmed35 <mahadahmed3528@gmail.com>
2026-09-23 19:30:07 +0200

    Bills quiz using map method

--- Array.js
+++ Array.js
@@ -25,21 +25,36 @@
 //     19, 300, 3775, 299, 36, 209, 148, 169, 299,
 //     6, 109, 20, 58, 139, 59, 3, 1, 139
 // ];
-// test.forEach(function(num){
-//     if(num/3 === 0){
-//         return num + 100;
+// test.forEach(function(item, index){
+//     if(item%3 === 0){
+//         item = item + 100;
+//         console.log("The updated element is "+item+" and actual is "+test[index]);
+//         test[index]=test[index]+100;
 //     }else{
-//         return "Not divisible by 3"
+//         console.log("Elemets " + item + " is not divisible by 3")
+        
 //     }
-//     console.log(test)
 // })
+//     console.log(test)
 
 
 //
-const myArray = [1, 3, 4, 5, 6, 6]
-function myAwesomeFunction(element, index, array){
-    console.log("Element: " + element + "Array: " + index);
+// const myArray = [1, 3, 4, 5, 6, 6]
+// function myAwesomeFunction(element, index, array){
+//     console.log("Element: " + element + "Array: " + index);
     
-}
+// }
 
-myArray.forEach(myAwesomeFunction)
+// myArray.forEach(myAwesomeFunction)
+var bills = [50.23, 19.12, 34.01,
+    100.11, 12.15, 9.90, 29.11, 12.99,
+    10.00, 99.22, 102.20, 100.10, 6.77, 2.22
+];
+var totals = bills.map(function(bill){
+    bill *= 1.15;
+   bill = bill.toFixed(2)
+   bill = Number(bill)
+    
+    return bill;
+})
+console.log(totals)
