6913ad5ed00464569dfc271854e0ee21e0989dfc
mahadahmed35 <mahadahmed3528@gmail.com>
2026-09-23 14:55:30 +0200

    Quidditch Cup Quiz

---
+++ Array.js
@@ -0,0 +1,12 @@
+// let numbers = [1, 2, 3];
+
+// numbers.push(4);
+
+// console.log(numbers)
+// Quidditch Cup Quiz.
+function hasEnoughPlayers(team){
+ return team.length >= 7? true: false;
+}
+
+var team = ["Oliver Wood", "Angelina Johnson", "Katie Bell", "Alicia Spinnet", "George Weasley", "Fred Weasley", "Harry Potter"];
+console.log(hasEnoughPlayers(team));
