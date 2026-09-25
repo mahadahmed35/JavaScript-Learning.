c4f6625e84e4fb44e91e72d464d88e3014f5a916
mahadahmed35 <mahadahmed3528@gmail.com>
2026-09-25 15:28:25 +0200

    Array methods find

--- arrayMethods.js
+++ arrayMethods.js
@@ -5,9 +5,25 @@
 // })
 
 //map(). Transform every item into something new. Convert product prices from USD to RWF.
-const students = [1,2,3,4,5]
- const doubled = students.map(function(students){
-    return students * 2
-})
+// const students = [1,2,3,4,5]
+//  const doubled = students.map(function(students){
+//     return students * 2
+// })
 
-console.log(doubled)
+// console.log(doubled)
+
+// Filter. Keep only items that match something new. Find products under $20.
+// const ages = [22, 11, 23, 55, 32, 31];
+// const adults = ages.filter(age => age >= 18)
+// console.log(adults)
+// find. Fin the first matching item. Find a user by ID.
+const users = [
+    { name: "Abdi", id: 1},
+    { name: "Abid", id: 2},
+    { name: "Abidd", id: 3},
+    { name: "Abida", id: 4},
+    { name: "Abide", id: 5},
+]
+const found = users.find( user => user.id === 2
+)
+console.log(found)
